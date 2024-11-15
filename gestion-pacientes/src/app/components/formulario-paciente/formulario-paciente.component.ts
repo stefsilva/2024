3d1a878fa631f8092fa66fafe1af7cad21fbbb40
stefsilva <114116859+stefsilva@users.noreply.github.com>
2024-11-15 15:13:45 -0300

    Add files via upload

---
+++ gestion-pacientes/src/app/components/formulario-paciente/formulario-paciente.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-paciente',
+  standalone: true,
+  imports: [],
+  templateUrl: './formulario-paciente.component.html',
+  styleUrl: './formulario-paciente.component.css',
+})
+export class FormularioPacienteComponent {}
---
+++ gestion-pacientes/src/app/components/tarjeta-paciente/tarjeta-paciente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tarjeta-paciente',
+  standalone: true,
+  imports: [],
+  templateUrl: './tarjeta-paciente.component.html',
+  styleUrl: './tarjeta-paciente.component.css'
+})
+export class TarjetaPacienteComponent {
+
+}
